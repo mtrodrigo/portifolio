e29d94bb0d41d23af4695219e8417e686eaec57d
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-02-13 19:00:09 -0300

    correção do componente sobre

--- src/components/sobre/Sobre.tsx
+++ src/components/sobre/Sobre.tsx
@@ -13,20 +13,19 @@ function Sobre() {
                     </div>
                     <div className={style.text_container}>
                         <p>
-                            Olá! Sou Rodrigo, um desenvolvedor web em transição de carreira. Tenho 39 anos e sou formado
+                            Olá! Sou Rodrigo, estou em transição de carreira para desenvolvedor web, tenho 39 anos e sou formado
                             em farmácia desde 2012, onde atuei como gestor farmacêutico por 13 anos. Desde janeiro de 2024,
                             decidi seguir meu sonho na área de programação e estou me dedicando a essa nova jornada.
                         </p>
                         <p>
                             Para me preparar, completei diversos cursos, incluindo iniciação em Front-End e JavaScript pela 
                             Trybe, Desenvolvimento Web (HTML 5, CSS 3, Bootstrap e JavaScript) com Jorge Sant Anna e Jamilton 
-                            Damasceno, além de um curso de Front-End pela DevMedia, que abrange HTML 5, CSS 3, JavaScript, 
-                            React e API. Recentemente, também finalizei um curso avançado de React com TypeScript na Udemy, 
-                            ministrado por Mathaus Fraga.
-  
+                            Damasceno, além do curso de Front-End pela DevMedia, que abrange HTML 5, CSS 3, JavaScript, 
+                            React e API. Recentemente, também finalizei o curso avançado de React com TypeScript na Udemy, 
+                            ministrado por Mathaus Fraga e sempre estou fazendo novos cursos para me aprimorar cada vez mais.
                         </p>
                         <p>
-                        Estou animado para aplicar minhas habilidades em projetos desafiadores e contribuir para o mundo da tecnologia!
+                            Estou animado para aplicar minhas habilidades em projetos desafiadores e contribuir para o mundo da tecnologia!
                         </p>
                     </div>
                 </div> 
