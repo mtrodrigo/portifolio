a7d8ca411b095b846593512bb960789c58f80bb6
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-04-28 17:12:00 -0300

    refatoração: remoção de código não utilizado e melhoria na formatação do componente Projetos

--- src/components/projetos/Projetos.tsx
+++ src/components/projetos/Projetos.tsx
@@ -1,44 +1,33 @@
-import styles from './Projetos.module.css'
-import colProjetos from '../../data/projeto'
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
-import { useRef } from 'react';
+import styles from "./Projetos.module.css";
+import colProjetos from "../../data/projeto";
 
 function Projetos() {
-  const carrocel = useRef<HTMLDivElement>(null)
-  const btnEsquerda = () => {
-    if (carrocel.current) {
-      carrocel.current.scrollLeft -= carrocel.current.offsetWidth
-    }
-  }
-  const btnDireita = () => {
-    if (carrocel.current) {
-      carrocel.current.scrollLeft += carrocel.current.offsetWidth
-    }
-  }
-
-    return (
-      <section id='projetos' className={styles.projetos}>
-        <div className={styles.projetos_container}>  
-          <div className={styles.projetos_titulo}>
-            <h2>Projetos</h2>
-          </div>
-          <div className={styles.cards_display}>
+  return (
+    <section id="projetos" className={styles.projetos}>
+      <div className={styles.projetos_container}>
+        <div className={styles.projetos_titulo}>
+          <h2>Projetos</h2>
+        </div>
+        <div className={styles.cards_display}>
           {colProjetos.map((projeto, index) => (
             <div key={index} className={styles.card}>
               <h4>{projeto.nome}</h4>
               <img src={projeto.image} alt={projeto.nome} />
               <p>{projeto.descricao}</p>
               <div className={styles.links_container}>
-                <a href={projeto.site} target='_blank'>Projeto</a>
-                <a href={projeto.codigo} target='_blank'>Código</a>
+                <a href={projeto.site} target="_blank">
+                  Projeto
+                </a>
+                <a href={projeto.codigo} target="_blank">
+                  Código
+                </a>
               </div>
             </div>
           ))}
-          </div>
-          </div>
-      </section>
-    )
-  }
-  
-  export default Projetos
-  
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Projetos;
